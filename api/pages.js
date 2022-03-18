f8165209de69d1a875bb08b9f0abdf799724c713
mikepreston17 <michael.n.preston@gmail.com>
2022-03-18 12:13:07 -0500

    Duplicate elimination added to merge statements

--- api/pages.js
+++ api/pages.js
@@ -6,36 +6,21 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 const axios = require("axios");
-const {
-  devmode,
-  prodmode,
-  environment,
-  deepFilter,
-  urlExists,
-} = require("../helpers");
+const { devmode } = require("../helpers");
 const { getUrl } = require("../helpers/wordpress");
-
-async function asyncPromAll(tasks = []) {
-  console.log("tasks", tasks);
-  const resultArray = await Promise.all(tasks);
-  for (let i = 0; i < resultArray.length; i++) {
-    console.log(resultArray[i]);
-  }
-  console.log("resultArray", resultArray);
-}
+const { createNode, createRelationship } = require("../helpers/neo");
 
 const extractLinks = (html = "") => {
   const dom = new JSDOM(html);
 
-  console.log("links found: ");
   let links = [];
   dom.window.document.querySelectorAll("a").forEach((link) => {
     links.push(link.href);
   });
 
-  dom.window.document.querySelectorAll("link").forEach((link) => {
-    links.push(link.rel);
-  });
+  // dom.window.document.querySelectorAll("link").forEach((link) => {
+  //   links.push(link.rel);
+  // });
 
   return links;
 };
@@ -46,52 +31,35 @@ const processWordpressResponse = async (response) => {
   const info = pages?.map((p) => {
     let links = extractLinks(p?.content?.rendered);
 
-    // console.log("p", p);
-    // console.log("links", links);
+    // devmode &&console.log("p", p);
+    // devmode && console.log("links", links);
 
     // Here's the most important info we want back.
     return {
-      excerpt: p?.excerpt?.rendered,
-      title: p?.title?.rendered,
-      author: p?.author,
-      categories: p?.categories,
-      tags: p?.tags,
-      slug: p?.slug,
-      status: p?.status,
-      description: p?.description,
-      comment_status: p?.comment_status,
-      links,
+      // api results, flattened a bit
+      Title: p?.title?.rendered,
+      Url: p?.link,
+      Slug: p?.slug,
+      Excerpt: p?.excerpt?.rendered,
+      Categories: p?.categories,
+      Tags: p?.tags,
+      Author: p?.author,
+      Status: p?.status,
+      Description: p?.description || "",
+      Comment_status: p?.comment_status,
+
+      // the links found in page
+      Links: links,
     };
   });
 
-  /**
-   * Dead link detection attempt... it works, but idk if we want to ping so much.  Might be best to let the users find them.  Then again...that's 1:1 or more.  Best to ask Ronnie.
-   */
-
-  //   console.log("info", info);
-  //   const content = response?.data?.[0]?.content?.rendered;
-  //   devmode && console.log("content ?", !!content);
-
-  //   const promises = ["https://www.google.com"]
-  //     .map((u) => urlExists(u))
-  //     .catch((err) => {
-  //       devmode && console.log("err", err);
-  //     });
-  //   const deadLinks = await asyncPromAll(promises).then((r) => {
-  //     console.log("r", r);
-  //   });
-  //   console.log("deadLinks", deadLinks);
-
-  return {
-    // pages,
-    ...info,
-  };
+  return info;
 };
 
 module.exports = async (req, res) => {
   devmode && console.log("query params: >> ", req?.query);
   const url = getUrl(req?.query);
-  console.log("url :>> ", url);
+  //  devmode && console.log("url :>> ", url);
 
   const response = await axios.get(url, {
     per_page: 3,
@@ -99,15 +67,52 @@ module.exports = async (req, res) => {
     _embed: true,
   });
 
-  console.log("teachings found : >> ", response?.data?.length);
+  //   console.log("teachings found : >> ", response?.data?.length);
 
-  const goodStuff = await processWordpressResponse(response);
-  console.log("goodStuff", goodStuff);
+  const pages = await processWordpressResponse(response);
+  devmode && console.log("pages", pages);
+
+  await createPageNodes(pages);
+
+  await createRelationship(pages);
 
   try {
-    // res.send(response?.data);
-    res.send(goodStuff);
+    res.send(pages);
   } catch (err) {
     res.send(err); // send the thrown error
   }
 };
+
+/**
+ * Creates pages in Neo4j db
+ * @param {pages} pages
+ */
+async function createPageNodes(pages) {
+  for (let index = 0; index < pages.length; index++) {
+    // const element = array[index];
+    const page = pages[index];
+    // Create a node, if not exists
+    let keys = Object.keys(page).filter(
+      (k) => !["Categories", "Tags"].includes(k)
+    );
+    console.log("keys", keys);
+    await createNode("Page", page, [keys]);
+  }
+}
+/**
+ * Dead link detection attempt... it works, but idk if we want to ping so much.  Might be best to let the users find them.  Then again...that's 1:1 or more.  Best to ask Ronnie.
+ */
+
+//   console.log("info", info);
+//   const content = response?.data?.[0]?.content?.rendered;
+//   devmode && console.log("content ?", !!content);
+
+//   const promises = ["https://www.google.com"]
+//     .map((u) => urlExists(u))
+//     .catch((err) => {
+//       devmode && console.log("err", err);
+//     });
+//   const deadLinks = await asyncPromAll(promises).then((r) => {
+//     console.log("r", r);
+//   });
+//   console.log("deadLinks", deadLinks);
---
+++ helpers/neo.js
@@ -0,0 +1,136 @@
+//https://memgraph.com/blog/cypher-cheat-sheet
+const uri = process.env.VITE_VERCEL_URI;
+const user = process.env.VITE_VERCEL_USER;
+const password = process.env.VITE_VERCEL_PASSWORD;
+import neo4j from "neo4j-driver";
+import { devmode } from "../helpers";
+export const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
+
+// Converts fields from a GET request to neo4j's dollar ($) placeholder format, e.g. { name: $name }
+function toNeoProps(props = null, keys = []) {
+  if (!props) throw new Error("properties passed to neo4j cannot be null!");
+  devmode && console.log("props", props);
+  return JSON.stringify(
+    Object.assign(
+      ...Object.entries(props)?.map(([key, value]) => {
+        return { [key]: `'${value || ""}'` };
+      })
+    )
+  )?.replaceAll(`"`, "");
+}
+
+//Execute Raw Queries here
+async function executeCypherQuery(statement, params = {}) {
+  try {
+    let session = driver.session();
+    const result = await session.run(statement, params);
+    await session.close();
+    return result;
+  } catch (error) {
+    console.log("error", error);
+    // throw error; // we are logging this error at the time of calling this method
+  }
+}
+executeCypherQuery;
+export const toObjectFormat = (records = []) => {
+  devmode && console.log("records", records);
+  return records?.map((r) => r["_fields"]?.map((r) => r?.properties)?.[0]);
+};
+
+/** Create A Node, update if exists, merge if not exactly matched  */
+export async function createNode(label = null, page = {}, requiredFields = []) {
+  if (!label) return ["Label cannot be null"];
+  if (!requiredFields?.length) throw Error(`requiredFields cannot be null`);
+
+  // devmode && console.log(`posting new ${label}...`);
+  console.log("requiredFields", requiredFields);
+  let fields = toNeoProps(page, requiredFields); // maybe I don't need to exclude here... see session.run()'s params
+  devmode && console.log("fields", fields);
+
+  // const conditions = `p.Title='${page?.Title}', p.Url='${page?.Url}'`;
+
+  let query = `MERGE (p:${label} ${fields}) 
+  RETURN p`;
+
+  console.log("query", query);
+  const result = await executeCypherQuery(query, {
+    ...fields,
+  });
+
+  const single = result?.records?.[0];
+  const node = single?.get(0);
+
+  devmode &&
+    console.log(`node`, node?.properties?.name || "< no node created >");
+
+  return node;
+}
+
+/**
+   If there are any links, relate them to the papers of the same url and title
+ *
+ * @param {first props of first node} props1
+ * @param {props of second node} props2
+ * @param {name of relationship} relationshipType
+ */
+export async function createRelationship(pages = []) {
+  for (let index = 0; index < pages.length; index++) {
+    const currentPage = pages[index];
+    devmode && console.log("page", currentPage);
+    const links = currentPage?.Links;
+
+    devmode && console.log("links", links);
+    for (let index = 0; index < links.length; index++) {
+      const link = links[index];
+
+      const rel = "LINKS_TO";
+      const conditions = `{ Title: '${currentPage?.Title}', Url: '${currentPage?.Url}'}`;
+      // console.log("conditions", conditions);
+
+      const query = `
+      match (from: Page ${conditions})
+      match (to: Page { Url: '${link}' } )
+      merge (to)<-[:${rel}]-(from)
+       `;
+
+      // const query = `match (from: Page ${conditions}), (to)
+      // ${whereClause}
+      // merge (to)<-[:${rel}]-(from)
+      //    `;
+
+      console.log("query", query);
+
+      await executeCypherQuery(query);
+    }
+  }
+}
+
+export async function getRecords(label = null, limit = 35) {
+  if (!label)
+    return [
+      `Nodes could not be found because you did not provide a label name`,
+    ];
+
+  let query = `MATCH (n:${label}) return n LIMIT ${limit}`;
+  devmode && console.log("query", query);
+  let results = await executeCypherQuery(query, {});
+  console.log("results", results);
+  return toObjectFormat(results?.records);
+}
+
+/** 
+ * 
+ * Other queries to implement
+ * 
+ * 
+ 
+show all nodes w/ relationships
+match (d)-[b]->(n)-[r]->(v) return d,b,n,r,v
+
+show nodes w/o any single relationships
+match ()-[b]-(n:Person)-[r]-(), (cc:Person) return n, type(b), type(r), cc
+
+
+
+
+*/
